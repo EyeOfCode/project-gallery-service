2b744c7ece374ab4c3b79e6e039baf1980de3f21
eyeofcode <champuplove@gmail.com>
2025-01-15 01:22:52 +0700

    chore: update swagger

--- docs/swagger.go
+++ docs/swagger.go
@@ -1,9 +1,19 @@
 package docs
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // UpdateSwaggerHost updates the Swagger host based on configuration
 func UpdateSwaggerHost(host, port string) {
+	env := os.Getenv("ENV")
+	prodHost := os.Getenv("DOMAIN")
+
+	if env == "production" {
+		SwaggerInfo.Host = prodHost
+		SwaggerInfo.Schemes = []string{"https"}
+	}
 	// Update the host directly in SwaggerInfo
 	SwaggerInfo.Host = fmt.Sprintf("%s:%s", host, port)
 }
